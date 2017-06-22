f2ca5ca45d9d663dde82a457499f593c0f1a56bf
premnathlearn <premlearnnew@gmail.com>
2017-06-22 19:54:38 +0530

    test 1

---
+++ buildscript/srcServer.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    path = require('path'),
+    open = require('open'),
+    port = 3000,
+    app = express();
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+app.listen(port, function (err) {
+  if(err)
+    console.log(err);
+  else
+    open('http://localhost:'+port);
+});
